b42eba87d5bd7cb7eca954f07e55935ef8dc05e9
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-05-28 19:51:44 +0900

    エラー回避のために一度コミット

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  def self.new_gest
+    find_or_create_by(email: 'gest@example.com') do |user|
+    user.password = SecureRandom.urlsafe_base64
+    end
+  end
+end
