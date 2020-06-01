b4dfd29fb552d9ccbd06f4937a4897cce1eb096b
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-06-01 18:16:32 +0900

    レビューの削除・更新機能を実装

--- spec/requests/reviews_request_spec.rb
+++ spec/requests/reviews_request_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Reviews", type: :request do
-  
+    before do
+      @user = FactoryBot.create(:user)
+      @place = FactoryBot.create(:place)
+      @review = FactoryBot.create(:review)
+    end
+
+    # describe "destroy" do
+    #   it "make sure succeessfully delete method" do
+    #     sign_in @user
+    #     delete place_review_path @place.id @review.id
+    #     expect(response).to have_http_status(200)
+    #   end
+    # end
+    #作成途中
 end
