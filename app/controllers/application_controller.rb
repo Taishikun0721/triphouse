72955d67da8715d550cc84a82f8d539615605bc9
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-06-13 16:40:48 +0900

    activestorageでアイコン画像を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-  protected
 
-  def cinfigure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[avator])
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[avator])
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[avator])
+    end
 end
