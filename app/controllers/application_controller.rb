32c716d0373594b7254052cc117d0d2a52b304be
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-06-14 23:56:46 +0900

    deviseのエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :show_user_icon
-
-  def show_user_icon
-    @user = User.find(current_user.id)
-  end
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[avator])
       devise_parameter_sanitizer.permit(:sign_up, keys: %i[avator])
     end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,11 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!
+
+  before_action :show_user_icon
+
+  def show_user_icon
+    @user = User.find(current_user.id) if user_signed_in?
+  end
 
   def index
     @places = Place.page(params[:page]).per(18)
