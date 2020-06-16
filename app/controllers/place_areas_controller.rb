6d92d0801579932dde084bc74de9bfcf3284ab28
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-06-16 22:18:28 +0900

    user情報をplacearacontrollerにも渡す

--- app/controllers/place_areas_controller.rb
+++ app/controllers/place_areas_controller.rb
@@ -1,6 +1,13 @@
 class PlaceAreasController < ApplicationController
 
   before_action :authenticate_user!, only: [:show]
+
+  before_action :show_user_icon
+
+  def show_user_icon
+    @user = User.find(current_user.id) if user_signed_in?
+  end
+
   def index
     @place_areas = PlaceArea.all
   end
