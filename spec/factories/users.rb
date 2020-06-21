9b03b2b31456ef172b31683711ce5afa3ae85a33
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-06-21 22:56:39 +0900

    user_specを追加とリファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "gest#{n}@example.com" }
     password { 'password' }
+    nickname { 'testuser' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'valid test' do
-    it 'has a valid factory' do
-      expect(FactoryBot.build(:user)).to be_valid
+
+  let(:user) { FactoryBot.build(:user) }
+
+  context 'バリデーションテスト' do
+    it 'ファクトリーボットの動作テスト' do
+      expect(user).to be_valid
     end
 
-    it 'valid without password' do
-      user = FactoryBot.build(:user, password: nil)
+    it 'パスワードの空白禁止' do
+      user.password = nil
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
 
-    it 'valid without email' do
-      user = FactoryBot.build(:user, email: nil)
+    it 'メールアドレスの空白禁止' do
+      user.email = nil
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
-    # it "valid over 50 length of email" do
-    #   user = FactoryBot.build(:user, email: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@example.com", password: SecureRandom.urlsafe_base64)
-    #   user.valid?
-    #   expect(user.errors[:email]).to include("Email is too long")
-    # end
-
-    it 'valid email is uniqueness' do
+    it 'メールアドレスがユニークな値であること' do
       FactoryBot.create(:user, email: 'test@example.com')
       other_user = FactoryBot.build(:user, email: 'test@example.com')
       other_user.valid?
       expect(other_user.errors[:email]).to include('has already been taken')
     end
+
+    it 'ニックネームが空白でないこと' do
+      user.nickname = nil
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
   end
 
-  context 'method test' do
-    it 'work login as a gestuser' do
+  context 'メソッドテスト' do
+    it '簡単ログイン機能が動作しているか' do
       expect(FactoryBot.create(:user, email: 'gest@example.com', password: SecureRandom.urlsafe_base64)).to be_valid
     end
   end
